69dfffb4b23c932e56d7d5eabbdadbab4baabf7f
Hyun_W <69100145+yehang218@users.noreply.github.com>
2022-09-11 21:46:30 +0900

    db 연결 및 회원가입

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, jsonify, request
+import os #디렉토리 절대 경로
+from flask import Flask, render_template, jsonify, request, redirect
+
+#로그인, 회원가입 관련
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from models import User
+
+#StarGAN
 from StarGAN import model
 from StarGAN import main as mainPy
 from StarGAN import solver
+
 app = Flask(__name__)
 
 ## HTML을 주는 부분
@@ -21,9 +30,30 @@ def forgotPW():
 def login():
    return render_template('login.html')
 
-@app.route('/signup')
+#회원가입
+@app.route('/signup', methods = ['GET', 'POST'])
 def signup():
-   return render_template('signup.html')
+   if request.method == 'GET':
+      return render_template('signup.html')
+   else :
+      name = request.form.get('name')
+      userid = request.form.get('userid')
+      password = request.form.get('password')
+      password_2 = request.form.get('password_2')
+
+      if not(name and userid and password and password_2) : 
+         return "모든 정보를 입력해주세요"
+      elif password != password_2 : 
+         return "비밀번호가 일치하지 않습니다."
+      else : 
+         usertable=User() #user_table 클래스
+         usertable.name = name
+         usertable.userid = userid
+         usertable.password = password
+            
+         db.session.add(usertable)
+         db.session.commit()
+         return redirect('/main')
 
 @app.route('/option_hair')
 def option_hair():
@@ -64,4 +94,17 @@ def run_model():
 
 ## 서버 연결
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5002,debug=True)
+   #데이터베이스
+      basedir = os.path.abspath(os.path.dirname(__file__)) #현재 파일이 있는 디렉토리 절대 경로
+      dbfile = os.path.join(basedir, 'db.sqlite') #데이터베이스 파일을 만든다
+
+      app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+      app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #사용자에게 정보 전달완료하면 teadown. 그 때마다 커밋=DB반영
+      app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #추가 메모리를 사용하므로 꺼둔다
+
+#    db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
+      db.init_app(app) #app설정값 초기화
+      db.app = app #Models.py에서 db를 가져와서 db.app에 app을 명시적으로 넣는다
+      db.create_all() #DB생성
+
+      app.run('0.0.0.0',port=5002,debug=True)
