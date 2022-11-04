15b4873c398dda0761077a157ab418387e75b13a
yehang218 <lovesh618@naver.com>
2022-11-04 14:40:07 +0900

    다운로드 완료 시 체크 표시 뜨게

--- app.py
+++ app.py
@@ -166,11 +166,13 @@ def result():
 @app.route('/run_model')
 def run_model():
    mainPy.main()
+   os.rename("static/images/completeImage.png", "static/images/cmImage.png")
    return render_template('result.html')
 
 @app.route('/result_download')
 def result_download():
    download_dir = "static/images/download"
+   os.rename("static/images/cmImage.png", "static/images/completeImage.png")
    if not os.path.exists(download_dir):
       os.makedirs(download_dir)
    while(True) :
